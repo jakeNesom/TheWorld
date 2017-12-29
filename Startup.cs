88366570cb9a8353145a5eb20da47b3039ecdcd3
Jake Nesom <jnesom@me.com>
2017-12-29 10:11:54 -0800

    commit

---
+++ Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TheWorld
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //order of options in Configure() are order of middleware execution
+
+            app.UseDefaultFiles();
+            /* 
+             * this option tells the web server to use whatever 'default' file types 
+             * are in the wwwroot folder
+              
+            */
+            app.UseStaticFiles();
+            /*
+             Tells the webserver to serve flat files instead of say, a string with html markup in it
+             */
+
+        }
+    }
+}
